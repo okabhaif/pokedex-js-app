0a40609053bf304d4a97f32e6861e91b91fe1b62
okabhaif <okabhaif@users.noreply.github.com>
2020-01-27 18:17:23 +0000

    Created elementary pokemon array / repository

---
+++ js/scripts.js
@@ -0,0 +1,109 @@
+var pokemon= [
+  //will draw out information re different pokemon for application
+
+    //var pokemon = {
+    {
+      name:'Pikachu',
+      type:'Electric',
+      weakness:'Ground',
+      height: 0.4m,
+    };
+
+    {
+      name:'Raichu',
+      type:'Electric',
+      weakness:'Ground',
+      height: 0.8m,
+    };
+
+    {
+      name: 'Bulbasaur',
+      type: ['Grass', 'Poison'],
+      weakness: ['Ground', 'Psychic', 'Flying', 'Fire', 'Ice'],
+      height: 0.7m,
+    };
+
+    {
+      name:'Ivysaur',
+      type: ['Grass', 'Poison'],
+      weakness: ['Ground', 'Psychic', 'Flying', 'Fire', 'Ice'],
+      height: 1.0m,
+    };
+
+    {
+      name:'Venusaur',
+      type: ['Grass', 'Poison'],
+      weakness: ['Ground', 'Psychic', 'Flying', 'Fire', 'Ice'],
+      height: 2.0m,
+    };
+
+    {
+      name:'Charmander',
+      type: 'Fire',
+      weakness: ['Ground', 'Rock', 'Water'],
+      height: 0.6m,
+    };
+
+    {
+      name:'Charmeleon',
+      type: 'Fire',
+      weakness: ['Ground', 'Rock', 'Water'],
+      height: 1.1m,
+    };
+
+    {
+      name:'Charizard',
+      type: 'Fire',
+      weakness: ['Ground', 'Rock', 'Water'],
+      height: 1.7m,
+    };
+
+    {
+      name:'Squirtle',
+      type: 'Water',
+      weakness: ['Grass', 'Electric'],
+      height: 0.5m,
+    };
+
+    {
+      name:'Wartortle',
+      type: 'Water',
+      weakness: ['Grass', 'Electric'],
+      height: 1.0m,
+    };
+
+    {
+      name:'Blastoise',
+      type: 'Water',
+      weakness: ['Grass', 'Electric'],
+      height: 1.6m,
+    };
+
+    {
+      name:'Eevee',
+      type: 'Normal',
+      weakness: ['Fighting'],
+      height: 0.3m,
+    };
+
+    {
+      name:'Vaporeon',
+      type: 'Water',
+      weakness: ['Grass', 'Electric'],
+      height: 1.0m,
+    };
+
+    {
+      name:'Jolteon',
+      type:'Electric',
+      weakness:'Ground',
+      height: 0.8m,
+    };
+
+    {
+      name:'Flareon',
+      type: 'Fire',
+      weakness: ['Ground', 'Rock', 'Water'],
+      height: 0.9m,
+    };
+]
