80d034472076d57ae5203ce5f2b94f5f963f15f6
okabhaif <okabhaif@users.noreply.github.com>
2020-01-28 19:46:22 +0000

    updated spacing and element order to make it flow better!

--- js/scripts.js
+++ js/scripts.js
@@ -4,120 +4,120 @@ var pokemon= [
 
     {
       name:'Pikachu',
-      type:'Electric',
-      weakness:'Ground',
+      type:' Electric',
+      weakness:' Ground',
       height: '0.4',
       evolutionStage: 'Basic',
     },
 
     {
       name:'Raichu',
-      type:'Electric',
-      weakness:'Ground',
+      type:' Electric',
+      weakness:' Ground',
       height: '0.8',
       evolutionStage: 'stageOne',
     },
 
     {
       name: 'Bulbasaur',
-      type: ['Grass', 'Poison'],
-      weakness: ['Ground', 'Psychic', 'Flying', 'Fire', 'Ice'],
+      type: [' Grass', ' Poison'],
+      weakness: [' Ground', ' Psychic', ' Flying', ' Fire', ' Ice'],
       height: '0.7',
       evolutionStage: 'Basic',
     },
 
     {
       name:'Ivysaur',
-      type: ['Grass', 'Poison'],
-      weakness: ['Ground', 'Psychic', 'Flying', 'Fire', 'Ice'],
+      type: [' Grass', ' Poison'],
+      weakness: [' Ground', ' Psychic', ' Flying', ' Fire', ' Ice'],
       height: '1.0',
       evolutionStage: 'stageOne',
     },
 
     {
       name:'Venusaur',
-      type: ['Grass', 'Poison'],
-      weakness: ['Ground', 'Psychic', 'Flying', 'Fire', 'Ice'],
+      type: [' Grass', ' Poison'],
+      weakness: [' Ground', ' Psychic', ' Flying', ' Fire', ' Ice'],
       height: '2.0',
       evolutionStage: 'stageTwo',
     },
 
     {
       name:'Charmander',
-      type: 'Fire',
-      weakness: ['Ground', 'Rock', 'Water'],
+      type: ' Fire',
+      weakness: [' Ground', ' Rock', ' Water'],
       height: '0.6',
       evolutionStage: 'Basic',
     },
 
     {
       name:'Charmeleon',
-      type: 'Fire',
-      weakness: ['Ground', 'Rock', 'Water'],
+      type: ' Fire',
+      weakness: [' Ground', ' Rock', ' Water'],
       height: '1.1',
       evolutionStage: 'stageOne',
     },
 
     {
       name:'Charizard',
-      type: 'Fire',
-      weakness: ['Ground', 'Rock', 'Water'],
+      type: ' Fire',
+      weakness: [' Ground', ' Rock', ' Water'],
       height: '1.7',
       evolutionStage: 'stageTwo',
     },
 
     {
       name:'Squirtle',
-      type: 'Water',
-      weakness: ['Grass', 'Electric'],
+      type: ' Water',
+      weakness: [' Grass', ' Electric'],
       height: '0.5',
       evolutionStage: 'Basic',
     },
 
     {
       name:'Wartortle',
-      type: 'Water',
-      weakness: ['Grass', 'Electric'],
+      type: ' Water',
+      weakness: [' Grass', ' Electric'],
       height: '1.0',
       evolutionStage: 'stageOne',
     },
 
     {
       name:'Blastoise',
-      type: 'Water',
-      weakness: ['Grass', 'Electric'],
+      type: ' Water',
+      weakness: [' Grass', ' Electric'],
       height: '1.6',
       evolutionStage: 'stageTwo',
     },
 
     {
       name:'Eevee',
-      type: 'Normal',
-      weakness: ['Fighting'],
+      type: ' Normal',
+      weakness: [' Fighting'],
       height: '0.3',
       evolutionStage: 'Basic',
     },
 
     {
       name:'Vaporeon',
-      type: 'Water',
-      weakness: ['Grass', 'Electric'],
+      type: ' Water',
+      weakness: [' Grass', ' Electric'],
       height: '1.0',
       evolutionStage: 'stageOne',
     },
 
     {
       name:'Jolteon',
-      type:'Electric',
-      weakness:'Ground',
+      type:' Electric',
+      weakness:' Ground',
       height: '0.8',
       evolutionStage: 'stageOne',
     },
 
     {
       name:'Flareon',
-      type: 'Fire',
-      weakness: ['Ground', 'Rock', 'Water'],
+      type: ' Fire',
+      weakness: [' Ground', ' Rock', ' Water'],
       height: '0.9',
       evolutionStage: 'stageOne',
     },
@@ -133,10 +133,10 @@ var pokemon= [
 
 
 for (var i = 0; i < pokemon.length; i++) {
-  document.write(pokemon[i].name +' \(Height\: '+pokemon[i].height+'m\; Type\: '+pokemon[i].type+'\; Weakness\: '+pokemon[i].weakness+'\)');
+  document.write(pokemon[i].name +' \(Type\:'+pokemon[i].type+'\; Weakness\:'+pokemon[i].weakness+'\; Height\: '+pokemon[i].height+'m\)');
 
   if (pokemon[i].height >= 1.0) {
-    document.write(' - You\'re HUGE!! <br>')
+    document.write(' - Wow, that\'s big!! <br>')
   }
   if (pokemon[i].height < 1.0) {
     document.write(' <br>')
